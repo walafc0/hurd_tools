58336dd2079975ff1cfa507a81dc81e6541bace9
walafc0 <devel@walafc0.org>
2014-07-01 12:11:40 +0200

    update ssh script

--- ssh.sh
+++ ssh.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if ! ps aux | grep "/usr/bin/sshd" | grep -v grep &> /dev/null; then
-  sudo systemctl start sshd
+if [ "$1" = "h" ]; then
+    if ! ps aux | grep "/usr/bin/sshd" | grep -v grep &> /dev/null; then
+	sudo systemctl start sshd
+    fi
+    if ! ifconfig | grep lo:0 &> /dev/null ; then
+	sudo ifconfig lo:0 172.20.0.1
+    fi
+    ssh -p 2222 user@127.0.0.1
+elif [ "$1" = "g" ]; then
+    if [ ! -f /var/run/sshd.pid ]; then
+	sudo /etc/rc.d/sshd start
+    fi
+    ssh -R 2222:127.0.0.1:22 walafc0@172.20.0.1
+else
+  echo "Wrong usage. $0 [h|g]"
 fi
-
-if ! ifconfig | grep lo:0 &> /dev/null ; then
-  sudo ifconfig lo:0 172.20.0.1
-fi
-
-ssh -p 2222 user@127.0.0.1
